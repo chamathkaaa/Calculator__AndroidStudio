e8538e0cc4ce75df3c65dfb1dce200e4ce990cb0
UviniC <uviniwijesinghe@gmail.com>
2019-08-10 09:56:04 +0530

    calculator app

---
+++ app/src/main/java/com/example/calculator/Main2Activity.java
@@ -0,0 +1,71 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    TextView num1, num2, answer;
+    Button btnPlus, btnMinus, btnMul, btnDiv;
+    String value1, value2;
+    int n1,n2,ans;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent intent = getIntent();
+
+        num1 = findViewById(R.id.editText1);
+        num2 = findViewById(R.id.editText2);
+        btnPlus = findViewById(R.id.button1);
+        btnMinus = findViewById(R.id.button2);
+        btnMul = findViewById(R.id.button3);
+        btnDiv = findViewById(R.id.button4);
+        answer = findViewById(R.id.answer);
+
+        value1 = intent.getStringExtra("FirstNumber");
+        value2 = intent.getStringExtra("SecondNumber");
+
+        n1 = Integer.parseInt(value1);
+        n2 = Integer.parseInt(value2);
+
+        num1.setText(value1);
+        num2.setText(value2);
+
+        btnPlus.setOnClickListener((View.OnClickListener) this);
+        btnMinus.setOnClickListener((View.OnClickListener) this);
+        btnMul.setOnClickListener((View.OnClickListener) this);
+        btnDiv.setOnClickListener((View.OnClickListener) this);
+
+    }
+
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.button1: ans = n1 + n2;
+                answer.setText(n1+ "+" +n2+ "=" +ans);
+                break;
+
+            case R.id.button2: ans = n1 - n2;
+                answer.setText(n1+ "-" +n2+ "=" +ans);
+                break;
+
+            case R.id.button3: ans = n1 * n2;
+                answer.setText(n1+ "*" +n2+ "=" +ans);
+                break;
+
+            case R.id.button4: ans = n1 / n2;
+                answer.setText(n1+ "/" +n2+ "=" +ans);
+                break;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnOk;
+    EditText text1,text2;
+    String n1,n2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOk = findViewById(R.id.btnOK);
+        text1 = findViewById(R.id.editText1);
+        text2 = findViewById(R.id.editText2);
+
+    }
+
+    protected void onResume()
+    {
+        super.onResume();
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                n1 = text1.getText().toString();
+                n2 = text2.getText().toString();
+
+                Intent i = new Intent(MainActivity.this, Main2Activity.class);
+                i.putExtra("FirstNumber", n1);
+                i.putExtra("SecondNumber", n2);
+
+
+                startActivity(i);
+            }
+        });
+    }
+}
